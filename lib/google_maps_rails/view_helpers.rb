5ebcca1cd8fcb5881030771c97d27a20429fa349
Luiz Picolo <luizpicolo@gmail.comm>
2015-11-03 22:38:27 -0300

    Fixed iframe

--- lib/google_maps_rails/view_helpers.rb
+++ lib/google_maps_rails/view_helpers.rb
@@ -2,7 +2,7 @@ module GoogleMapsRails
   module ViewHelpers
     def embed_google_maps(lat, long, width = 500, height = 500)
       if lat != '' && long != ''
-        "<iframe src = 'https://maps.google.com/maps?q=#{lat},#{long}&hl=es;z=14&amp;output=embed' frameborder='0' style='border:0' width='#{width}' height='#{height}' allowfullscreen></iframe>"
+        "<iframe src = 'https://maps.google.com/maps?q=#{lat},#{long}&amp;output=embed' frameborder='0' style='border:0' width='#{width}' height='#{height}' allowfullscreen></iframe>"
       else
         "latitude or longitude not found"
       end
--- spec/google_maps_rails_spec.rb
+++ spec/google_maps_rails_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GoogleMapsRails do
   it 'return iframe with current params' do
     embed = embed_google_maps('-20.262197', '-54.799805', '250', '250')
-    expect(embed).to eq("<iframe src = 'https://maps.google.com/maps?q=-20.262197,-54.799805&hl=es;z=14&amp;output=embed' frameborder='0' style='border:0' width='250' height='250' allowfullscreen></iframe>")
+    expect(embed).to eq("<iframe src = 'https://maps.google.com/maps?q=-20.262197,-54.799805&amp;output=embed' frameborder='0' style='border:0' width='250' height='250' allowfullscreen></iframe>")
   end
 
   it 'return message error with wrong params' do
