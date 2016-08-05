3577126da50a79c8b463657a9b3c30abbc788997
Luiz Picolo <luizpicolo@gmail.com>
2016-08-05 15:58:35 -0400

    update tests

--- google_maps_rails.gemspec
+++ google_maps_rails.gemspec
@@ -17,8 +17,8 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency 'rails'
-  spec.add_development_dependency "rspec"
+  spec.add_development_dependency 'rails', '~> 0'
+  spec.add_development_dependency 'rspec', '~> 0'
   spec.add_development_dependency "bundler", "~> 1.8"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- spec/google_maps_rails_spec.rb
+++ spec/google_maps_rails_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
 describe GoogleMapsRails do
-  it 'return iframe with current params' do
-    embed = embed_google_maps('-20.262197', '-54.799805', '250', '250')
-    expect(embed).to eq("<iframe src = 'https://maps.google.com/maps?q=-20.262197,-54.799805&amp;output=embed' frameborder='0' style='border:0' width='250' height='250' allowfullscreen></iframe>")
+  it 'return iframe with latitude and longitude params ' do
+    embed = embed_google_maps(lat: '-20.262197', long: '-54.799805', width: '250', height: '250')
+    expect(embed).to eq("<iframe src='https://maps.google.com/maps?q=-20.262197,-54.799805&amp;output=embed' frameborder='0' style='border:0' width='250' height='250' allowfullscreen></iframe>")
   end
 
-  it 'return message error with wrong params' do
-    embed = embed_google_maps('', '', '250', '250')
-    expect(embed).to eq("latitude or longitude not found")
+  it 'return iframe with address params' do
+    embed = embed_google_maps(address: 'name address, 5555', width: '250', height: '250')
+    expect(embed).to eq("<iframe src='https://maps.google.com/maps?q=name%20address%2C%205555&amp;output=embed' frameborder='0' style='border:0' width='250' height='250' allowfullscreen></iframe>")
+  end
+
+  it 'return error message if wrong params' do
+    embed = embed_google_maps()
+    expect(embed).to eq("latitude, longitude or address not found. See documentation")
   end
 end
